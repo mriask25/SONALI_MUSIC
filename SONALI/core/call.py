9e6c5c3904e083dd56a9bee699e7c1a5f7861959
mriask25 <tom125shelby@gmail.com>
2025-07-01 14:56:01 +0530

    Update call.py

--- SONALI/core/call.py
+++ SONALI/core/call.py
@@ -1,190 +1,65 @@
-import asyncio
-import os
-from datetime import datetime, timedelta
-from typing import Union
+import asyncio import os from datetime import datetime, timedelta from typing import Union
 
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import (
-    AlreadyJoinedError,
-    NoActiveGroupCall,
-    TelegramServerError,
-)
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
-from pytgcalls.types.stream import StreamAudioEnded
+from pyrogram import Client from pyrogram.types import InlineKeyboardMarkup from pytgcalls import PyTgCalls from pytgcalls.exceptions import ( AlreadyJoinedError, NoActiveGroupCall, TelegramServerError, ) from pytgcalls.types import Update from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo from pytgcalls.types.stream import StreamAudioEnded
 
-import config
-from SONALI import LOGGER, YouTube, app
-from SONALI.misc import db
-from SONALI.utils.database import (
-    add_active_chat,
-    add_active_video_chat,
-    get_lang,
-    get_loop,
-    group_assistant,
-    is_autoend,
-    music_on,
-    remove_active_chat,
-    remove_active_video_chat,
-    set_loop,
-)
-from SONALI.utils.exceptions import AssistantErr
-from SONALI.utils.formatters import check_duration, seconds_to_min, speed_converter
-from SONALI.utils.inline.play import stream_markup, telegram_markup
-from SONALI.utils.stream.autoclear import auto_clean
-from SONALI.utils.thumbnails import get_thumb
-from strings import get_string
+import config from SONALI import LOGGER, YouTube, app from SONALI.misc import db from SONALI.utils.database import ( add_active_chat, add_active_video_chat, get_lang, get_loop, group_assistant, is_autoend, music_on, remove_active_chat, remove_active_video_chat, set_loop, ) from SONALI.utils.exceptions import AssistantErr from SONALI.utils.formatters import check_duration, seconds_to_min, speed_converter from SONALI.utils.inline.play import stream_markup, telegram_markup from SONALI.utils.stream.autoclear import auto_clean from SONALI.utils.thumbnails import get_thumb from strings import get_string
 
-autoend = {}
-counter = {}
+class Call: def init(self): self.userbot1 = Client("RAUSHANAss1", api_id=config.API_ID, api_hash=config.API_HASH, session_string=str(config.STRING1)) self.one = PyTgCalls(self.userbot1)
 
+self.userbot2 = Client("RAUSHANAss2", api_id=config.API_ID, api_hash=config.API_HASH, session_string=str(config.STRING2))
+    self.two = PyTgCalls(self.userbot2)
 
-async def _clear_(chat_id):
-    db[chat_id] = []
-    await remove_active_video_chat(chat_id)
-    await remove_active_chat(chat_id)
+    self.userbot3 = Client("RAUSHANXAss3", api_id=config.API_ID, api_hash=config.API_HASH, session_string=str(config.STRING3))
+    self.three = PyTgCalls(self.userbot3)
 
+    self.userbot4 = Client("RAUSHANXAss4", api_id=config.API_ID, api_hash=config.API_HASH, session_string=str(config.STRING4))
+    self.four = PyTgCalls(self.userbot4)
 
-class Call:
-    def __init__(self):
-        self.userbot1 = Client(
-            name="RAUSHANAss1",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING1),
-        )
-        self.one = PyTgCalls(self.userbot1)
+    self.userbot5 = Client("RAUSHANAss5", api_id=config.API_ID, api_hash=config.API_HASH, session_string=str(config.STRING5))
+    self.five = PyTgCalls(self.userbot5)
 
-        self.userbot2 = Client(
-            name="RAUSHANAss2",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING2),
-        )
-        self.two = PyTgCalls(self.userbot2)
+async def stop_stream(self, chat_id: int):
+    assistant = await group_assistant(self, chat_id)
+    try:
+        await assistant.leave_group_call(chat_id)
+    except:
+        pass
 
-        self.userbot3 = Client(
-            name="RAUSHANXAss3",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING3),
-        )
-        self.three = PyTgCalls(self.userbot3)
+async def skip_stream(self, chat_id: int, link: str, video: Union[bool, str] = None):
+    assistant = await group_assistant(self, chat_id)
+    stream = AudioVideoPiped(link, audio_parameters=HighQualityAudio(), video_parameters=MediumQualityVideo()) if video else AudioPiped(link, audio_parameters=HighQualityAudio())
+    await assistant.change_stream(chat_id, stream)
 
-        self.userbot4 = Client(
-            name="RAUSHANXAss4",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING4),
-        )
-        self.four = PyTgCalls(self.userbot4)
+async def join_call(self, chat_id: int, link, video: Union[bool, str] = None):
+    assistant = await group_assistant(self, chat_id)
+    language = await get_lang(chat_id)
+    _ = get_string(language)
+    stream = AudioVideoPiped(link, audio_parameters=HighQualityAudio(), video_parameters=MediumQualityVideo()) if video else AudioPiped(link, audio_parameters=HighQualityAudio())
+    try:
+        await assistant.join_group_call(chat_id, stream)
+    except NoActiveGroupCall:
+        raise AssistantErr(_["call_8"])
+    except AlreadyJoinedError:
+        raise AssistantErr(_["call_9"])
+    except TelegramServerError:
+        raise AssistantErr(_["call_10"])
+    await add_active_chat(chat_id)
+    await music_on(chat_id)
+    if video:
+        await add_active_video_chat(chat_id)
 
-        self.userbot5 = Client(
-            name="RAUSHANAss5",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING5),
-        )
-        self.five = PyTgCalls(self.userbot5)
-
-    async def pause_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.pause_stream(chat_id)
-
-    async def resume_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.resume_stream(chat_id)
-
-    async def stop_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        try:
-            await _clear_(chat_id)
-            await assistant.leave_group_call(chat_id)
-        except:
-            pass
-
-    async def join_call(
-        self,
-        chat_id: int,
-        original_chat_id: int,
-        link,
-        video: Union[bool, str] = None,
-        image: Union[bool, str] = None,
-    ):
-        assistant = await group_assistant(self, chat_id)
-        language = await get_lang(chat_id)
-        _ = get_string(language)
-
-        stream = (
-            AudioVideoPiped(
-                link, audio_parameters=HighQualityAudio(), video_parameters=MediumQualityVideo()
-            )
-            if video
-            else AudioPiped(link, audio_parameters=HighQualityAudio())
-        )
-
-        try:
-            await assistant.join_group_call(chat_id, stream)
-        except NoActiveGroupCall:
-            raise AssistantErr(_["call_8"])
-        except AlreadyJoinedError:
-            raise AssistantErr(_["call_9"])
-        except TelegramServerError:
-            raise AssistantErr(_["call_10"])
-
-        await add_active_chat(chat_id)
-        await music_on(chat_id)
-        if video:
-            await add_active_video_chat(chat_id)
-        if await is_autoend():
-            counter[chat_id] = {}
-            users = len(await assistant.get_participants(chat_id))
-            if users == 1:
-                autoend[chat_id] = datetime.now() + timedelta(minutes=1)
-
-    async def stream_call(self, link):
-        assistant = await group_assistant(self, config.LOGGER_ID)
-        await assistant.join_group_call(config.LOGGER_ID, AudioVideoPiped(link))
-        await asyncio.sleep(0.2)
-        await assistant.leave_group_call(config.LOGGER_ID)
-
-    async def start(self):
-        LOGGER(__name__).info("Starting PyTgCalls Clients...\n")
-        if config.STRING1:
-            await self.one.start()
-        if config.STRING2:
-            await self.two.start()
-        if config.STRING3:
-            await self.three.start()
-        if config.STRING4:
-            await self.four.start()
-        if config.STRING5:
-            await self.five.start()
-
-    async def decorators(self):
-        @self.one.on_closed_voice_chat()
-        @self.two.on_closed_voice_chat()
-        @self.three.on_closed_voice_chat()
-        @self.four.on_closed_voice_chat()
-        @self.five.on_closed_voice_chat()
-        @self.one.on_left()
-        @self.two.on_left()
-        @self.three.on_left()
-        @self.four.on_left()
-        @self.five.on_left()
-        async def stream_services_handler(_, chat_id: int):
-            await self.stop_stream(chat_id)
-
-        @self.one.on_stream_end()
-        @self.two.on_stream_end()
-        @self.three.on_stream_end()
-        @self.four.on_stream_end()
-        @self.five.on_stream_end()
-        async def stream_end_handler(client, update):
-            if isinstance(update, StreamAudioEnded):
-                await self.stop_stream(update.chat_id)
-
+async def start(self):
+    LOGGER(__name__).info("Starting PyTgCalls Client...")
+    if config.STRING1:
+        await self.one.start()
+    if config.STRING2:
+        await self.two.start()
+    if config.STRING3:
+        await self.three.start()
+    if config.STRING4:
+        await self.four.start()
+    if config.STRING5:
+        await self.five.start()
 
 RAUSHAN = Call()
+
